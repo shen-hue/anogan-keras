a32383fe2bd39f795e0349990fed41e674ff5112
shen-hue <73641445+shen-hue@users.noreply.github.com>
2021-12-17 03:02:18 +0100

    Update SHAP.py

--- SHAP.py
+++ SHAP.py
@@ -10,16 +10,15 @@ X_test_standard = (X_test-np.min(X_test))/(np.max(X_test)-np.min(X_test))
 test_pred = np.load('result_artificial/test_pred.npy').reshape(-1,n)
 test_pred_standard = (test_pred-np.min(X_test))/(np.max(X_test)-np.min(X_test))
 rec_err = np.linalg.norm(X_test_standard-test_pred_standard, axis=1)
-result = pd.DataFrame(data=None,index=range(len(X_test_standard)),columns=['shapvalue'])
+result = np.zeros((X_test.shape[0],X_test.shape[1],X_test.shape[1]))
 # calculate SHAP value
 for j in range(len(X_test_standard)):
     idx = j
-    df = pd.DataFrame(data=test_pred_standard[idx], index= range(n),columns=['reconstruction_loss'])
     loaded_model = anogan.anomaly_detector()
     loaded_model.load_weights('result_artificial/weights/test_1_' + str(j) + '.h5')
     explainer = shap.DeepExplainer(loaded_model, X_test_standard[:250,:])
     shap_values = explainer.shap_values(X_test_standard[idx,:].reshape(-1,n))
-    result.loc[j, 'shapvalue'] = shap_values
+    result[j,:,:] = shap_values
 
 np.save('result_artificial_classification/shapvalue', result)
 
